31583b69624a38d12254a49575209da6ad5e57be
OkelloSam <samuelokello929@gmail.com>
2024-07-02 16:20:45 +0300

    feat: add bottom navigation
    Added bottom navigation bar with two items: Products and Checkout.
    Created navigation routes and composables for each screen.
    Integrated Product
    ViewModel and ProductRepository for the Products screen.

--- app/src/main/java/com/samuelokello/shopspot/MainActivity.kt
+++ app/src/main/java/com/samuelokello/shopspot/MainActivity.kt
@@ -5,22 +5,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
+import androidx.compose.runtime.remember
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.samuelokello.shopspot.repository.ProductRepository
+import com.samuelokello.shopspot.ui.checkout.CheckoutScreen
+import com.samuelokello.shopspot.ui.navigation.Screens
 import com.samuelokello.shopspot.ui.navigation.bottom_navigation.BottomNavigationBar
 import com.samuelokello.shopspot.ui.products.ProductScreen
 import com.samuelokello.shopspot.ui.products.ProductViewModel
+import com.samuelokello.shopspot.ui.products.ProductViewModelFactory
 import com.samuelokello.shopspot.ui.theme.ShopSpotTheme
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 class MainActivity : ComponentActivity() {
-    private val viewModel: ProductViewModel by viewModels()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             val navController = rememberNavController()
+
             ShopSpotTheme {
                 BottomNavigationBar()
             }
