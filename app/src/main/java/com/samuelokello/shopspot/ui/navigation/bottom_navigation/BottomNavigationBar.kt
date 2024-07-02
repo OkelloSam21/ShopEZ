d39997eca472a754473cac98ed317a3012cdab1f
OkelloSam <samuelokello929@gmail.com>
2024-07-02 15:54:51 +0300

    feat: add checkout screen
    Added a Checkout screen composable and integrated it into the bottom navigation bar.
    Updated the bottom navigation bar's container color.
    
    Used the ProductViewModel in the CheckoutScreen.

--- app/src/main/java/com/samuelokello/shopspot/ui/navigation/bottom_navigation/BottomNavigationBar.kt
+++ app/src/main/java/com/samuelokello/shopspot/ui/navigation/bottom_navigation/BottomNavigationBar.kt
@@ -1,5 +1,6 @@
 package com.samuelokello.shopspot.ui.navigation.bottom_navigation
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -19,11 +20,15 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.samuelokello.shopspot.repository.ProductRepository
+import com.samuelokello.shopspot.ui.checkout.CheckoutScreen
 import com.samuelokello.shopspot.ui.navigation.Screens
 import com.samuelokello.shopspot.ui.products.ProductScreen
 import com.samuelokello.shopspot.ui.products.ProductViewModel
 import com.samuelokello.shopspot.ui.products.ProductViewModelFactory
+import com.samuelokello.shopspot.ui.theme.primaryContainerLight
+import com.samuelokello.shopspot.ui.theme.secondaryContainerLight
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun BottomNavigationBar() {
     var navigationSelectedItem by rememberSaveable { mutableIntStateOf(0) }
@@ -35,9 +40,12 @@ fun BottomNavigationBar() {
     // Create ViewModelFactory
     val factory = remember { ProductViewModelFactory(productRepository) }
 
+    val viewModel: ProductViewModel = viewModel(factory = factory)
     Scaffold(
         bottomBar = {
-            NavigationBar(containerColor = MaterialTheme.colorScheme.surface) {
+            NavigationBar(
+                containerColor = secondaryContainerLight
+            ) {
                 BottomNavigationItem().bottomNavigationItems().forEachIndexed { index, item ->
                     NavigationBarItem(
                         selected = index == navigationSelectedItem,
@@ -63,11 +71,11 @@ fun BottomNavigationBar() {
             modifier = Modifier.padding(innerPadding),
         ) {
             composable(Screens.Products.route) {
-                val viewModel: ProductViewModel = viewModel(factory = factory)
                 ProductScreen(navController = navController, viewModel = viewModel)
             }
             composable(Screens.Checkout.route) {
                 // Checkout screen composable
+                CheckoutScreen(navController = navController, viewModel = viewModel)
             }
         }
     }
