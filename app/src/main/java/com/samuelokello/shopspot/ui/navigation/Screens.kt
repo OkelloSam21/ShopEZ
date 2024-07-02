a88ba23d3f028d5ad7936be4c8f6603a1ef535cf
OkelloSam <samuelokello929@gmail.com>
2024-07-02 16:16:11 +0300

    **feat: add order placed screen**
    Added a new screen for displaying order confirmation.
    Introduced a new route for the order placed screen.
    
    No functional changes were made.

--- app/src/main/java/com/samuelokello/shopspot/ui/navigation/Screens.kt
+++ app/src/main/java/com/samuelokello/shopspot/ui/navigation/Screens.kt
@@ -3,4 +3,5 @@ package com.samuelokello.shopspot.ui.navigation
 sealed class Screens (val route : String){
     object Products : Screens("products_route")
     object Checkout : Screens("checkout_screen")
+    object OrderPlaced: Screens("order_placed_screen")
 }
