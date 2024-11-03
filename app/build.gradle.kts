541e8ad09006252e316b68709c1b590e7637ec4c
okellosamuel <samuelokello929@gmail.com>
2024-11-03 04:42:32 +0300

    project upgrade

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.samuelokello.shopspot"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.samuelokello.shopspot"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
