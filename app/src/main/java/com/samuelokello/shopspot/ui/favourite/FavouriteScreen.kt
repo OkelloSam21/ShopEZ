25024b949855e66ddd0a48cf6cc2b5eef44ce3ca
okellosamuel <samuelokello929@gmail.com>
2024-11-03 04:52:46 +0300

    feat: Search feat: Profile feat: Favourite

---
+++ app/src/main/java/com/samuelokello/shopspot/ui/favourite/FavouriteScreen.kt
@@ -0,0 +1,10 @@
+package com.samuelokello.shopspot.ui.favourite
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.samuelokello.shopspot.ui.navigation.Screens
+
+@Composable
+fun FavouriteScreen(modifier: Modifier = Modifier) {
+
+}
