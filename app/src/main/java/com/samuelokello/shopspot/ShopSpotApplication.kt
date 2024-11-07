598d74fdf8510b5fc408d2322f2950d9f844bd9d
okellosamuel <samuelokello929@gmail.com>
2024-11-07 09:14:12 +0300

    enhancement add application class

---
+++ app/src/main/java/com/samuelokello/shopspot/ShopSpotApplication.kt
@@ -0,0 +1,13 @@
+package com.samuelokello.shopspot
+
+import android.app.Application
+import com.samuelokello.shopspot.data.DefaultAppContainer
+import com.samuelokello.shopspot.data.ShopSpotContainer
+
+class ShopSpotApplication : Application(){
+    lateinit var container: ShopSpotContainer
+    override fun onCreate() {
+        super.onCreate()
+        container = DefaultAppContainer()
+    }
+}
